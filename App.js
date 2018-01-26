449a3f31041f8340f6acff3be44b7b11d5a82bfe
HashedDan <georgedanielmangum@gmail.com>
2018-01-26 15:31:27 -0600

    component work

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+class Greeting extends React Component {
+  render () {
+    return (
+      <Text>Good to see ya!</Text>
+    );
+  }
+}
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>This is a test here.</Text>
+        <Text>Hello World.</Text>
+        <Greeting/>
       </View>
     );
   }
