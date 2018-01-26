5632e19a2de4566f5bfe4c3490652621e2e719c6
HashedDan <georgedanielmangum@gmail.com>
2018-01-26 16:47:56 -0600

    playing with layout

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-class Greeting extends React Component {
+class Greeting extends Component {
+  constructor(props) {
+    super(props);
+    if (this.props.name == 'Daniel') {
+      this.state = {name:'Master'};
+    }
+    else {
+      this.state= {name: 'imposter'};
+    }
+  }
+
   render () {
     return (
-      <Text>Good to see ya!</Text>
+      <Text style={this.props.style}>Hey there {this.state.name}.</Text>
     );
   }
 }
 
-export default class App extends React.Component {
+class TopBox extends Component {
+  render () {
+    return (
+      <Text style={{color: 'white'}}>Top Half</Text>
+    );
+  }
+}
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World.</Text>
-        <Greeting/>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
@@ -24,7 +43,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
