c62659bfc00bf1cfae672599e3e7424c5b070272
HashedDan <georgedanielmangum@gmail.com>
2018-02-06 10:40:03 -0600

    signup

--- app/views/auth/signup.js
+++ app/views/auth/signup.js
@@ -1,8 +1,40 @@
 import React, {Component} from 'react';
 import { View, Button, Image, Alert, Text } from 'react-native';
 import { Icon, Header } from 'react-native-elements';
+import * as firebase from "firebase";
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      response: ""
+    };
+
+    this.login = this.login.bind(this);
+  }
+
+  async signup() {
+  	try {
+  		await firebase.auth().createUserWithEmailAndPassword(email, password);
+
+  		this.setState({
+  		  response: "Successful Signup!"
+  		});
+
+  		setTimeout(() => {
+  		  this.props.navigation.navigate('MainNav')
+  		}, 1500);
+  	}
+  	catch (error) {
+  		this.setState({
+  		  response: error.toString(),
+  		})
+  	}
+  }
+
   render () {
     return (
       <View style={{ flex: 1, backgroundColor: '#fff'}}>
