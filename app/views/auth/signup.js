f3103bc7d904022c94e9830d5bbfc34394dbd5b3
HashedDan <georgedanielmangum@gmail.com>
2018-02-06 10:42:39 -0600

    signup UI

--- app/views/auth/signup.js
+++ app/views/auth/signup.js
@@ -37,8 +37,32 @@ class SignUp extends Component {
 
   render () {
     return (
-      <View style={{ flex: 1, backgroundColor: '#fff'}}>
-      	<Text>SignUp</Text>
+      <View style={{ flex: 1, backgroundColor: '#000', alignItems: 'center' }}>
+        <Card
+          title='Welcome!'>
+          <Text style={{marginBottom: 10}}>
+            Please login below to view your profile.
+          </Text>
+          <FormLabel>Email</FormLabel>
+          <View>
+            <FormInput
+              containerStyle={{ width: 200}}
+              onChangeText={(email) => this.setState({email})} />
+          </View>
+          <FormLabel>Password</FormLabel>
+          <View>
+            <FormInput
+              containerStyle={{ width: 200}}
+              onChangeText={(password) => this.setState({password})} />
+          </View>
+          <Button
+            icon={{name: 'code'}}
+            backgroundColor='#03A9F4'
+            fontFamily='Futura'
+            buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0, marginTop: 10 }}
+            onPress={() => this.signup}
+            title='Log In' />
+        </Card>
       </View>
     );
   }
