f7e1cdbc7f0df7e0a38f149997dcfbb3a2e8c1cf
HashedDan <georgedanielmangum@gmail.com>
2018-02-19 15:09:56 -0600

    titles on flat list

--- app/views/main/builder.js
+++ app/views/main/builder.js
@@ -55,7 +55,7 @@ class Builder extends Component {
     }
 
     this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(newItems),
+      dataSource: newItems,
       items: newItems
     });
   }
