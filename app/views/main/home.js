440670344083c1f68db08f87698494e4ac70ad36
HashedDan <georgedanielmangum@gmail.com>
2018-02-19 13:17:20 -0600

    update xcode, new action buttons... iPhone X ruining headers...

--- app/views/main/home.js
+++ app/views/main/home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import { View, Button, Image, Text, ListView, StatusBar, Alert } from 'react-native';
+import { View, Button, Image, Text, ListView, StatusBar, Alert, StyleSheet } from 'react-native';
 import { Icon, Header, Card, List, ListItem } from 'react-native-elements';
+import ActionButton from 'react-native-action-button';
 import * as firebase from "firebase";
 import Firebase from "../../firebase";
 
@@ -64,32 +65,34 @@ class Home extends Component {
   render () {
     console.log(firebase.auth().currentUser.email);
     return (
-      <View style={{ flex: 1, backgroundColor: '#fff'}}>
-        <Header
-          leftComponent={ <Icon
-                            name='menu'
-                            color='#fff'
-                            onPress={() => this.props.navigation.navigate('DrawerToggle')} /> }
-          centerComponent={ /*<Image
-                              style={{width: 55, height: 20}}
-                              source={require('../../assets/logo.png')} />*/ 
-                              <Text style={{color: 'white', fontSize: 30 }}>Draftnik</Text>
-                            }
-          // rightComponent={ <Icon
-          //                   type='entypo'
-          //                   name='squared-plus'
-          //                   color='#fff'
-          //                   onPress={() => this.props.navigation.navigate('Builder')} /> }
-          outerContainerStyles={{ backgroundColor: '#000' }}
-        />
+      <View style={{ flex: 1, backgroundColor: '#fff', paddingTop: 20}}>
         <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Hot Players Right Now</Text>
         <ListView
           renderRow={this.renderRow}
           dataSource={this.state.dataSource}
         />
+
+        <ActionButton buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+            <Icon name='home' style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => this.props.navigation.navigate('Profile')}>
+            <Icon name='person' style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
+            <Icon name='build' style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </ActionButton>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
+});
 module.exports = Home;
